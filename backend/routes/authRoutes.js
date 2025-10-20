16cf4ddb798ad3876bff003da095dda52fc7e5a0
msagr <mehulsagrawal33@gmail.com>
2025-10-20 11:27:24 +0530

    feat: add logout api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,6 +6,7 @@ import { loginLimiter } from "../middleware/apiLimiter.js";
 import newAccessToken from "../controllers/auth/refreshTokenController.js";
 import resendEmailVerificationToken from "../controllers/auth/resendVerifyEmailController.js";
 import {resetPassword, resetPasswordRequest} from "../controllers/auth/passwordResetController.js";
+import logoutUser from "../controllers/auth/logoutController.js";
 
 const router = express.Router();
 
@@ -16,5 +17,6 @@ router.get("/new_access_token", newAccessToken);
 router.post("/resend_email_token", resendEmailVerificationToken);
 router.post("/reset_password_request", resetPasswordRequest);
 router.post("/reset_password", resetPassword);
+router.post("/logout", logoutUser);
 
 export default router;
