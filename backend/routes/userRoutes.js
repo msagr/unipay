b33863ecd882a11c3000787a113e32600d8f9ae5
msagr <mehulsagrawal33@gmail.com>
2025-10-20 16:46:08 +0530

    feat: add delete user profile api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ import express from "express";
 import getUserProfile from "../controllers/user/getUserProfile.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
+import deleteAccount from "../controllers/user/deleteAccount.js";
 
 const router = express.Router();
 
 router.route("/profile").get(checkAuth, getUserProfile);
 router.patch("/profile", checkAuth, updateUserProfile);
+router.delete("/profile", checkAuth, deleteAccount);
 
 export default router;
