2cdb7d3baf158368c1612e80bfb1bb95ddb22811
msagr <mehulsagrawal33@gmail.com>
2025-10-20 15:35:45 +0530

    feat: create update user profile api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import getUserProfile from "../controllers/user/getUserProfile.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
+import updateUserProfile from "../controllers/user/updateUserProfile.js";
 
 const router = express.Router();
 
 router.route("/profile").get(checkAuth, getUserProfile);
+router.patch("/profile", checkAuth, updateUserProfile);
 
 export default router;
