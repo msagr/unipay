f8489916293cfc6fae2b5d3315a705567203842a
msagr <mehulsagrawal33@gmail.com>
2025-10-20 10:27:03 +0530

    feat: add new access token backend api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,11 +3,13 @@ import registerUser from "../controllers/auth/registerController.js";
 import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
 import loginUser from "../controllers/auth/loginController.js";
 import { loginLimiter } from "../middleware/apiLimiter.js";
+import newAccessToken from "../controllers/auth/refreshTokenController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
 router.post("/login", loginLimiter, loginUser);
+router.get("/new_access_token", newAccessToken);
 
 export default router;
